afade3f3d9c6d0aaae23189604857d216f1bf21e
EightEnexy <enexyges@gmail.com>
2021-05-04 19:28:43 +0200

    should wok

--- src/controller.cpp
+++ src/controller.cpp
@@ -5,6 +5,7 @@ Controller::Controller()
    micro = std::make_unique<Microscopy>();
 }
 
+
 bool Controller::addLense(int type, QString position, QString vergency, QString deflectionXAxis, QString deflectionZAxis) {
     std::cout << "Hello" << std::endl;
     return true;
